2c71d9e5adb00771aaaba1b6273072d8a99071f8
RugkeyPro <rugkeypro@users.noreply.github.com>
2026-08-12 22:33:31 +0800

    feat: expand supported iOS range to 17.0 - 27.x

--- mond/mond.swift
+++ mond/mond.swift
@@ -42,7 +42,7 @@ struct mond: App {
                 .environmentObject(state)
                 .onAppear() {
                     if !is_supported() {
-                        Alertinator.shared.alert(title: "Not supported!", body: "Your iOS version may not be supported by mond.\nMond supports iOS 26.0 - 27.x.")
+                        Alertinator.shared.alert(title: "Not supported!", body: "Your iOS version may not be supported by mond.\nMond supports iOS 17.0 - 27.x.")
                     }
                 }
                 .overlay {
