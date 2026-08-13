b428f544223c35c0ea0647eef1a252bc2be29f2e
RugkeyPro <rugkeypro@users.noreply.github.com>
2026-08-13 13:30:36 +0800

    fix: add required init() for BADownloaderExtension protocol conformance

--- BAExtension/BAExtensionDelegate.swift
+++ BAExtension/BAExtensionDelegate.swift
@@ -2,6 +2,7 @@ import BackgroundAssets
 
 @available(iOS 16.4, *)
 class BAExtensionDelegate: BADownloaderExtension {
+    required init() {}
     func backgroundDownload(_ download: BADownload, failedWithError error: Error) {}
     func backgroundDownload(_ download: BADownload, finishedWithFileURL fileURL: URL) {}
 }
