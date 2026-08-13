bdc66dbaae45bbacb90245498a27537005575d48
RugkeyPro <rugkeypro@users.noreply.github.com>
2026-08-13 09:35:08 +0800

    fix: rewrite BAExtensionDelegate in Objective-C to ensure zero Swift framework requirement & clean compilation across iOS versions

---
+++ BAExtension/BAExtensionDelegate.h
@@ -0,0 +1,17 @@
+//
+//  BAExtensionDelegate.h
+//  BAExtension
+//
+//  Created by roooot on 2026-08-13.
+//
+
+#import <Foundation/Foundation.h>
+#import <BackgroundAssets/BackgroundAssets.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BAExtensionDelegate : NSObject <BADownloaderExtension>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BAExtension/BAExtensionDelegate.m
@@ -0,0 +1,18 @@
+//
+//  BAExtensionDelegate.m
+//  BAExtension
+//
+//  Created by roooot on 2026-08-13.
+//
+
+#import "BAExtensionDelegate.h"
+
+@implementation BAExtensionDelegate
+
+- (void)backgroundDownload:(BADownload *)download failedWithError:(NSError *)error {
+}
+
+- (void)backgroundDownload:(BADownload *)download finishedWithFileURL:(NSURL *)fileURL {
+}
+
+@end
